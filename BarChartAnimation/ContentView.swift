9fe081f229d89367d07c1069a2aed6b8d5ef843b
Dinesh <34174856+dinesh2910@users.noreply.github.com>
2020-06-05 14:52:35 -0500

    Initial Commit

--- BarChartAnimation/ContentView.swift
+++ BarChartAnimation/ContentView.swift
@@ -9,11 +9,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var pickerSelectedItem = 0
+    @State var dataPoins: [CGFloat] = [50, 100, 150]
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(#colorLiteral(red: 0.3378706319, green: 0.937254902, blue: 0.9115050101, alpha: 1)).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Calory Intake")
+                    .font(.system(size: 35))
+                    .fontWeight(.heavy)
+                
+                Picker(selection: $pickerSelectedItem, label: Text ("")) {
+                    Text("Morning").tag(0)
+                    Text("Afternoon").tag(1)
+                    Text("Evening").tag(2)
+                }.pickerStyle(SegmentedPickerStyle())
+                    .padding(.horizontal, 25)
+                
+                
+                HStack (spacing: 20) {
+                     BarView(topCapsuleValue: dataPoins[pickerSelectedItem])
+                     BarView(topCapsuleValue: dataPoins[pickerSelectedItem])
+                     BarView(topCapsuleValue: dataPoins[pickerSelectedItem])
+                     BarView(topCapsuleValue: dataPoins[pickerSelectedItem])
+                     BarView(topCapsuleValue: dataPoins[pickerSelectedItem])
+                     BarView(topCapsuleValue: dataPoins[pickerSelectedItem])
+                     BarView(topCapsuleValue: dataPoins[pickerSelectedItem])
+                }.padding(.top, 24)
+            }
+        }
     }
 }
 
+struct BarView: View {
+    var topCapsuleValue: CGFloat
+    
+    var body: some View {
+    VStack {
+        ZStack (alignment: .bottom){
+            Capsule().frame(width: 30, height: 200)
+                .foregroundColor(Color(#colorLiteral(red: 0.4756349325, green: 0.4756467342, blue: 0.4756404161, alpha: 0.5345141267)))
+            Capsule().frame(width: 30, height: topCapsuleValue)
+                .foregroundColor(.white)
+        }
+        Text("Su").padding(.top, 8)
+    }
+  }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
